a147fa247d1effad031dc9e0c3ac1364d8330e83
P-iknow <apricotsoul@gmail.com>
2020-03-28 17:36:37 +0900

    practice: 1-일반 객체를 interface 로 타입 설정하기

--- velopert/ts-practicce/src/practice.ts
+++ velopert/ts-practicce/src/practice.ts
@@ -57,3 +57,26 @@ const shapes: Shape[] = [new circle(5), new Rectangle(10, 5)];
 shapes.forEach(shape => {
 	console.log(shape.getArea());
 })
+
+// 일반 객체를 interface 로 타입 설정하기 
+
+interface Person {
+	name: string;
+	age?: number; 
+}
+
+interface Developer extends Person {
+	skills: string[];
+}
+
+const person: Person = {
+	name: '김사람',
+	age: 20
+};
+
+const expert: Developer = {
+	name: '김개발',
+	skills: ['javascript', 'react']
+};
+
+const people: Person[] = [person, expert];
