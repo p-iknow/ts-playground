f2de9a047e5880dc6cb4f39dd5b789a13b2b19a1
P-iknow <apricotsoul@gmail.com>
2020-03-28 19:11:07 +0900

    practice: 1-Generic

--- velopert/ts-practicce/src/practice.ts
+++ velopert/ts-practicce/src/practice.ts
@@ -108,3 +108,37 @@ const people: People = [person, expert];
 type Color = 'red' | 'orange' | 'yellow';
 const color: Color = 'red';
 const colors: Color[] = ['red', 'orange'];
+
+// Generic 
+
+// 타입 추론이 깨지는 상황
+
+/*
+
+function merge(a: any, b: any): any {
+	return {
+		...a,
+		...b
+	};
+}
+
+const merged = merge({ foo: 1 }, { bar: 1 });
+
+*/
+
+function merge<A, B>(a: A, b: B): A & B {
+	return {
+		...a,
+		...b
+	}
+}
+
+const merged = merge({ foo: 1 } , { bar: 1 });
+
+function wrap<T>(param: T) {
+	return {
+		param
+	}
+}
+
+const wrapped = wrap(10);
