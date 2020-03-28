7e74402e00bc07076e70e24a5209f10e6c3f59e5
P-iknow <apricotsoul@gmail.com>
2020-03-28 17:31:11 +0900

    practice: 1-interface 사용해보기

--- velopert/ts-practicce/src/practice.ts
+++ velopert/ts-practicce/src/practice.ts
@@ -1,6 +1,5 @@
 let count = 0;
 count += 1;
-count = "ts";
 
 const message: string = "hellow world";
 
@@ -9,7 +8,6 @@ const doen: boolean = true;
 const numbers: number [] = [1, 2, 3]; 
 const messages: string [] = ["hello", "world"];
 
-messages.push(1);
 
 let mightBeUndefined: string | undefined = undefined;
 let nullableNumber: number | null = null;
@@ -24,3 +22,38 @@ function sum(x: number, y: number): number {
 function sumArray(numbers: number[]): number {
 	return numbers.reduce((acc, current) => acc + current, 0);
 }
+
+interface Shape {
+	getArea(): number;
+}
+
+class circle implements Shape {
+
+	radius: number;
+
+	constructor(radius: number) {
+		this.radius = radius;
+	}
+
+	getArea() {
+		return this.radius * this.radius * Math.PI;
+	}
+}
+
+class Rectangle implements Shape {
+	width: number;
+	height: number;
+	constructor(width: number, height: number) {
+		this.width = width;
+		this.height = height;
+	}
+	getArea() {
+		return this.width * this.height;
+	}
+}
+
+const shapes: Shape[] = [new circle(5), new Rectangle(10, 5)];
+
+shapes.forEach(shape => {
+	console.log(shape.getArea());
+})
