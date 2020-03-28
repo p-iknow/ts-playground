de0cc73113996d16e62357168a9a5e9f5465fcab
P-iknow <apricotsoul@gmail.com>
2020-03-28 17:15:46 +0900

    init: velopert 1강 강의 세팅

---
+++ velopert/ts-practicce/src/practice.ts
@@ -0,0 +1,22 @@
+let count = 0;
+count += 1;
+count = "ts";
+
+const message: string = "hellow world";
+
+const doen: boolean = true; 
+
+const numbers: number [] = [1, 2, 3]; 
+const messages: string [] = ["hello", "world"];
+
+messages.push(1);
+
+let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: number | null = null;
+
+let color: 'red' | 'orange' | 'yellow' = 'red';
+
+
+function sum(x: number, y: number): number {
+	return x + y;
+}
