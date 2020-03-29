e70f531dbe802f0efe2215a075daf3e0396b9f2c
P-iknow <apricotsoul@gmail.com>
2020-03-29 10:47:17 +0900

    practice: 타입스크립트로 리액트 Hooks 사용하기

--- velopert/ts-react-tutorial/src/App.tsx
+++ velopert/ts-react-tutorial/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Greetings from './Greetings';
+import MyForm from './MyForm';
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
   };
-  return <Greetings name="Hello" onClick={onClick} />;
+  return <MyForm onSubmit={onSubmit} />;
 };
 
 export default App;
