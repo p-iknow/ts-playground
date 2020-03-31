f0da1ade30dd7916756dcd341195eb9a5c729e59
P-iknow <apricotsoul@gmail.com>
2020-03-31 22:03:53 +0900

    practice: typesafe-actions로 리덕스 모듈 리팩토링하기

--- velopert/ts-react-redux-tutorial/src/modules/counter.ts
+++ velopert/ts-react-redux-tutorial/src/modules/counter.ts
@@ -1,19 +1,25 @@
-const INCREASE = 'counter/INCREASE' as const;
-const DECREASE = 'counter/DECREASE' as const;
-const INCREASE_BY = 'counter/INCREASE_BY' as const;
+
+import {
+  createAction,
+  ActionType,
+  createReducer
+} from 'typesafe-actions';
 
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
-export const increaseBy = (diff: number) => ({
-  type: INCREASE_BY,
-  payload: diff
-});
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+const INCREASE_BY = 'counter/INCREASE_BY';
 
-type CounterAction =
-  | ReturnType<typeof increase>
-  | ReturnType<typeof decrease>
-	| ReturnType<typeof increaseBy>;
+export const increase = createAction(INCREASE)();
+// () => ({ type: INCREASE })
+export const decrease = createAction(DECREASE)();
+// () => ({ type: DECREASE }) 
+export const increaseBy = createAction(INCREASE_BY)<number>();
+// (payload: number) => ({ type: INCREASE_BY, payload })
+
+const actions = { increase, decrease, increaseBy };
+
+type CounterAction = ActionType<typeof actions>;
 
 type CounterState = {
 	count: number;
@@ -23,17 +29,30 @@ const initialState: CounterState = {
 	count: 0
 };
 
-function counter(state: CounterState = initialState, action: CounterAction) {
-  switch (action.type) {
-    case INCREASE:
-      return { count: state.count + 1 };
-    case DECREASE:
-      return { count: state.count - 1 };
-    case INCREASE_BY:
-      return { count: state.count + action.payload };
-    default:
-      return state;
-  }
-}
+//function counter(state: CounterState = initialState, action: CounterAction) {
+//  switch (action.type) {
+//    case INCREASE:
+//      return { count: state.count + 1 };
+//    case DECREASE:
+//      return { count: state.count - 1 };
+//    case INCREASE_BY:
+//      return { count: state.count + action.payload };
+//    default:
+//      return state;
+//  }
+//}
+
+//const counter = createReducer<CounterState, CounterAction>(initialState, {
+//  [INCREASE]: state => ({ count: state.count + 1 }),
+//  [DECREASE]: state => ({ count: state.count - 1 }),
+//  [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload })
+//});
+
+const counter = createReducer<CounterState, CounterAction>(initialState)
+  .handleAction(increase, state => ({ count: state.count + 1 }))
+  .handleAction(decrease, state => ({ count: state.count - 1 }))
+  .handleAction(increaseBy, (state, action) => ({
+    count: state.count + action.payload
+  }));
 
 export default counter;
